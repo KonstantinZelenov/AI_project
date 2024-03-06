a4efff93e35eab8ff69538387052697963bcfc27
KonstantinZelenov <131335805+KonstantinZelenov@users.noreply.github.com>
2024-03-06 15:06:23 +0400

    toucnmove

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-/*
+
 const text = document.getElementById("prompt");
 const changeText = document.getElementsByClassName("btn");
 function change () {
     text.innerText = "New text";
 };
-changeText[0].onclick = change;
-*/
+changeText[0].ontouchmove = change;
+
 
 const item = document.querySelector("div");
 function changeColor() {
